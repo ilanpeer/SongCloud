f8a99f8188121a006ee1619ba1d31b78fed48276
Ilanp <ilan.web@gmail.com>
2017-03-29 08:41:12 +0300

    updtae index.html back to apps folders

--- app/components/player.js
+++ app/components/player.js
@@ -6,7 +6,7 @@ export default function Player() {
     <div>
       <p>Player component</p>
       <img alt="Song thumbnail" src="#"/>
-      <span>Song Title:</span>
+      <span>Song Title: boom boom</span>
       <audio controls="controls">
         <source src="#" type="audio/mpeg" />
         Your browser does not support the audio element.
